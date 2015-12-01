1a1872fe0d5046b59fd03ee7ebba6452b4c74efc
unknown <138066824@8B83943F.maple.fg.rbc.com>
2015-12-01 12:08:06 -0800

    initial checkin

---
+++ SqlContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Hackcom.Dapper
+{
+    public class SqlContext : IRepoContext
+    {
+        private string _connectionString;
+        public SqlContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public IDbConnection CeateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
